665e0589de553d203bfbaff0f22fbfdb7713aa57
leesou <1181902665@qq.com>
2021-05-21 15:57:36 +0800

    对已实现的worker表操作进行了测试

--- src/test/java/team/combinatorics/midtermproject/DatabaseTest.java
+++ src/test/java/team/combinatorics/midtermproject/DatabaseTest.java
@@ -8,8 +8,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import team.combinatorics.midtermproject.dao.DepartmentDao;
 import team.combinatorics.midtermproject.dao.ProductDao;
 import team.combinatorics.midtermproject.dao.UserDao;
+import team.combinatorics.midtermproject.dao.WorkerDao;
 import team.combinatorics.midtermproject.model.po.DepartmentPO;
 import team.combinatorics.midtermproject.model.po.UserPO;
+import team.combinatorics.midtermproject.model.po.WorkerPO;
 
 import java.util.List;
 
@@ -26,6 +28,9 @@ public class DatabaseTest {
     @Autowired
     DepartmentDao departmentDao;
 
+    @Autowired
+    WorkerDao workerDao;
+
     @Test
     public void userDaoTest() {
         UserPO userPO = UserPO.builder()
@@ -38,8 +43,7 @@ public class DatabaseTest {
     }
 
     @Test
-    public void departmentDaoTest()
-    {
+    public void departmentDaoTest() {
         DepartmentPO departmentPO = DepartmentPO.builder()
                                         .departmentName("testDep")
                                         .build();
@@ -58,4 +62,40 @@ public class DatabaseTest {
             departmentDao.deleteByPrimaryKey(dep.getDid());
     }
 
+
+    @Test
+    public void workerDaoTest() {
+        DepartmentPO departmentPO = DepartmentPO.builder()
+                .departmentName("testDep")
+                .build();
+        departmentDao.insert(departmentPO);
+
+        List<DepartmentPO> departmentPOList;
+        departmentPOList = departmentDao.selectAll();
+        Integer did = null;
+        for(DepartmentPO dep:departmentPOList)
+            if(dep.getDepartmentName().equals(departmentPO.getDepartmentName()))
+                did = dep.getDid();
+
+        WorkerPO workerPO = WorkerPO.builder()
+                                .workerName("leesou")
+                                .salary(114.514)
+                                .did(did)
+                                .email("1919@8.10")
+                                .phoneNumber("123456")
+                                .build();
+        workerDao.insert(workerPO);
+        workerPO.setWid(1);
+        System.out.println(workerPO.getWid());
+
+        workerPO.setWorkerName("notmad");
+        workerDao.update(workerPO);
+
+        WorkerPO answer = workerDao.selectByPrimaryKey(workerPO.getWid());
+        System.out.println(answer.getWorkerName());
+
+        // used for test correctness
+        answer.setDid(2);
+        workerDao.update(answer);
+    }
 }
