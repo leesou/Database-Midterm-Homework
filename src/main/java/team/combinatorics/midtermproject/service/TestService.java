9cddb09d37338d6fcf2f0283e2ab97ab2c638246
leesou <1181902665@qq.com>
2021-05-26 19:42:10 +0800

    删除TestDao，Service，Controller

--- src/main/java/team/combinatorics/midtermproject/service/TestService.java
+++
@@ -1,7 +0,0 @@
-package team.combinatorics.midtermproject.service;
-
-import team.combinatorics.midtermproject.model.dto.TestDTO;
-
-public interface TestService {
-    TestDTO getUserByID(int id);
-}
