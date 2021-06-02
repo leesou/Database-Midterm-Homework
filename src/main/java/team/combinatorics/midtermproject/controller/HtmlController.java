13d25e0cb4ff6d9c6394f33a540e3ac08ec3571d
leesou <1181902665@qq.com>
2021-06-02 15:49:38 +0800

    添加了用于挂载网站的Controller

---
+++ src/main/java/team/combinatorics/midtermproject/controller/HtmlController.java
@@ -0,0 +1,21 @@
+package team.combinatorics.midtermproject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+public class HtmlController {
+
+    @RequestMapping("/login")
+    public String logIn() {
+        return "login";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
